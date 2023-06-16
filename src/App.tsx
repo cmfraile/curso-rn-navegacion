4228bb89f6631ef1a30b5ed8813d371b451f0d1f
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-06-16 10:34:03 +0200

    cascaron

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import Main from './screens/Main.screen';
+
+const App = () => <SafeAreaView style={{flex:1}}><Main/></SafeAreaView>
+
+export default App
---
+++ src/screens/Main.screen.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+const Main = () => <Text>Hola mundo</Text>;
+
+export default Main
