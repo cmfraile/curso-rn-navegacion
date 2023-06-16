06277385aca8e900893e82cbebc4e4cf09e73ecf
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-06-16 11:02:45 +0200

    103

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import Main from './screens/Main.screen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const App = () => <SafeAreaView style={{flex:1}}><Main/></SafeAreaView>
+const Stack = createNativeStackNavigator()
+
+//Clase 103 en caso de errores
+
+const App = () => 
+  <SafeAreaView style={{flex:1}}>
+
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen name='Home' component={Main}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+    
+  </SafeAreaView>
 
 export default App
