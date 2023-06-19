304bbc52a16ca459bc83e3a59ea7d6d99a4f2ba3
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-06-19 10:35:12 +0200

    114

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,23 +1,36 @@
 import 'react-native-gesture-handler';
-import { DrawerContentComponentProps, DrawerContentScrollView, createDrawerNavigator } from '@react-navigation/drawer';
+import { DrawerContentComponentProps, DrawerContentScrollView, DrawerItem, createDrawerNavigator } from '@react-navigation/drawer';
 import FirstScreen from '../screens/Second/FirstScreen';
 import SecondScreen from '../screens/Second/SecondScreen';
-import { Image, Text , View } from 'react-native';
+import { Image, Text , TouchableOpacity, View } from 'react-native';
 
 
 const Drawer = createDrawerNavigator();
 
 const CustomIcon = () => 
   <Image
-    source={require('../assets/burger.png')}
-    style={{width:150,height:150}}
+    source={require('../assets/shark.png')}
+    style={{width:150,height:150,marginTop:30}}
   />
 
 const InsideDrawer = (props:DrawerContentComponentProps) => {
 
   return (
-    <DrawerContentScrollView style={{flex:1,backgroundColor:'red'}}>
-      <View style={{flex:1,backgroundColor:'blue'}}><CustomIcon></CustomIcon></View>
+    <DrawerContentScrollView>
+      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <CustomIcon/>
+      </View>
+      <DrawerItem
+        label='first'
+        onPress={() => props.navigation.navigate('First')}
+      />
+      <DrawerItem
+        label='second'
+        onPress={() => props.navigation.navigate('Second')}
+      />
+      <TouchableOpacity>
+        <Text>Custom BTN</Text>
+      </TouchableOpacity>
     </DrawerContentScrollView>
   )
 
@@ -25,7 +38,7 @@ const InsideDrawer = (props:DrawerContentComponentProps) => {
 
 const DrawerApp = () => {
   return (
-    <Drawer.Navigator 
+    <Drawer.Navigator
       initialRouteName='First'
       drawerContent={ (props) => <InsideDrawer {...props}/> }
       screenOptions={{
